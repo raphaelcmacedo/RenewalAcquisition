273e1ccf83a7392bb927a1a6949ee522ffe2bafb
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-02-08 14:35:29 -0200

    Controle transacional e exclusão de quotes repetidas

--- ImportRenewals/Repositories/QuoteRepository.cs
+++ ImportRenewals/Repositories/QuoteRepository.cs
@@ -17,40 +17,84 @@ namespace ImportRenewals.Repositories
 
         public override void Add(Quote quote)
         {
-            //Generate Hash
-            if (string.IsNullOrEmpty(quote.Hash))
+            using (DbContextTransaction t = base.DbContext.Database.BeginTransaction())
             {
-                quote.Hash = Util.GenerateHash();
-            }
-            foreach(QuoteLine quoteLine in quote.QuoteLines)
-            {
-                if (string.IsNullOrEmpty(quoteLine.Hash))
-                {
-                    quoteLine.Hash = Util.GenerateHash();
-                }
-            }
 
-            //Avoid vendor changes
-            base.DbContext.Entry(quote.Vendor).State = EntityState.Unchanged;
+                //Delete old version
+                this.RemoveOldQuote(quote);
 
-            //Add quote
-            DbContext.Set<Quote>().Add(quote);
-            
-            //Add the VRFValues
-            foreach (QuoteLine quoteLine in quote.QuoteLines)
-            {
-                foreach (VRFValue vrfValue in quoteLine.VRFValues)
+                //Generate Hash
+                if (string.IsNullOrEmpty(quote.Hash))
                 {
-                    //Avoid VRF changes
-                    base.DbContext.Entry(vrfValue.VRF).State = EntityState.Unchanged;
-                    vrfValue.QuoteLine = quoteLine;
-                    DbContext.Set<VRFValue>().Add(vrfValue);
+                    quote.Hash = Util.GenerateHash();
                 }
+                foreach (QuoteLine quoteLine in quote.QuoteLines)
+                {
+                    if (string.IsNullOrEmpty(quoteLine.Hash))
+                    {
+                        quoteLine.Hash = Util.GenerateHash();
+                    }
+                }
+
+                //Avoid vendor changes
+                base.DbContext.Entry(quote.Vendor).State = EntityState.Unchanged;
+
+                //Add quote
+                DbContext.Set<Quote>().Add(quote);
+
+                //Add the VRFValues
+                foreach (QuoteLine quoteLine in quote.QuoteLines)
+                {
+                    foreach (VRFValue vrfValue in quoteLine.VRFValues)
+                    {
+                        //Avoid VRF changes
+                        base.DbContext.Entry(vrfValue.VRF).State = EntityState.Unchanged;
+                        vrfValue.QuoteLine = quoteLine;
+                        DbContext.Set<VRFValue>().Add(vrfValue);
+                    }
+                }
+
+                DbContext.SaveChanges();
+                t.Commit();
             }
-            
-            DbContext.SaveChanges();
         }
 
+        public Quote FindByNumber(string quoteNumber)
+        {
+            QuoteContext context = (QuoteContext)DbContext;
+            return (from q in context.Quotes
+                    where q.QuoteNumber.Equals(quoteNumber)
+                    select q).FirstOrDefault();
+        }
+
+        public void RemoveOldQuote(Quote newQuote)
+        {
+            Quote old = this.FindByNumber(newQuote.QuoteNumber);
+            if (old != null)
+            {
+                //Delete the lines
+                QuoteContext context = (QuoteContext)DbContext;
+
+                List<QuoteLine> lines = new List<QuoteLine>();
+                lines.AddRange(old.QuoteLines);
+
+                foreach (QuoteLine quoteLine in lines)
+                {
+                    //Delete the VRFs
+                    List<VRFValue> vrfValues = new List<VRFValue>();
+                    vrfValues.AddRange(quoteLine.VRFValues);
+                    foreach (VRFValue vrfValue in vrfValues)
+                    {
+                        DbContext.Set<VRFValue>().Remove(vrfValue);
+                    }
+
+                    DbContext.Set<QuoteLine>().Remove(quoteLine);
+                }
+
+                //Delete the quotes
+                DbContext.Set<Quote>().Remove(old);
+            }
+        }
 
     }
 }
